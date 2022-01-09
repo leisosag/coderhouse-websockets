f610bbff3db2b0fdf680c6a3773cd2de5e978ed9
Leila Sosa Gonzalez <leisosa93@gmail.com>
2022-01-09 14:55:28 -0300

    added chat

--- public/js/main.js
+++ public/js/main.js
@@ -1,23 +1,51 @@
+// const dayjs = require('dayjs');
 let socket = io.connect();
 
 socket.on('products', function (data) {
-	console.log('data', data);
-	render(data);
+	renderProducts(data);
 });
 
-const render = (data) => {
+socket.on('messages', function (data) {
+	renderMessages(data);
+});
+
+const renderProducts = (data) => {
+	let row = `
+		<th>Nombre</th>
+		<th>Precio</th>
+		<th>Thumbnail</th>
+	`;
+	row += data
+		.map((item) => {
+			return `
+			<tr>
+				<td>${item.product.name}</td>
+				<td>${item.product.price}</td>
+				<td class='table-image'><img class='img-thumbnail' src='${item.product.thumbnail}' alt='image'/></td>
+			</tr>
+		`;
+		})
+		.join(' ');
+	document.getElementById('products').innerHTML = row;
+};
+
+const renderMessages = (data) => {
 	let html = data
 		.map((item) => {
 			return `
-				<tr>
-					<td>${item.product.name}</td>
-					<td>${item.product.price}</td>
-					<td class='table-image'><img class='img-thumbnail' src='${item.product.thumbnail}' alt='image'/></td>
-				</tr>
+				<div class='message col-md-12 mt-3 p-1'>
+					<div class='row d-flex mt-2'>
+						<p class='font-weight-bold mb-0 ml-4'>${item.message.email}</p>
+						<p class='text-time mb-0 mx-2'>${item.message.time}</p>
+					</div>
+					<div class='row d-flex mt-2'>
+					<p class='ml-4 mb-0 pb-1'>${item.message.text}</p>
+					</div>
+				</div>
 			`;
 		})
 		.join(' ');
-	document.getElementById('products').innerHTML = html;
+	document.getElementById('messages').innerHTML = html;
 };
 
 const addProduct = (e) => {
@@ -27,5 +55,17 @@ const addProduct = (e) => {
 		thumbnail: document.getElementById('thumbnail').value,
 	};
 	socket.emit('new-product', product);
+	return false;
+};
+
+const sendMessage = (e) => {
+	let message = {
+		email: document.getElementById('email').value,
+		text: document.getElementById('message').value,
+		time: dayjs().format('DD/MM/YYYY HH:mm:ss'),
+	};
+	socket.emit('new-message', message);
+	document.getElementById('message').value = '';
+	document.getElementById('message').focus();
 	return false;
 };
--- server.js
+++ server.js
@@ -3,37 +3,43 @@ const app = express();
 const server = require('http').Server(app);
 const io = require('socket.io')(server);
 const path = require('path');
+const fs = require('fs');
 let products = [];
+let messages = [];
+
+const productsFile = './products.txt';
+const messagesFile = './messages.txt';
 
 app.set('view engine', 'pug');
 app.set('views', path.resolve(__dirname, './views'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-// app.use(express.static('public/'));
 app.use('*/js', express.static('public/js'));
 app.use('*/css', express.static('public/css'));
 
 io.on('connection', function (socket) {
 	console.log('Conectado!');
-	socket.emit('products', products); // emitir todos los productos a un cliente nuevo
+	socket.emit('products', products);
+	socket.emit('messages', messages);
 
 	socket.on('new-product', function (data) {
-		products.push({ socketId: socket.id, product: data }); // agregar producto a array
-		io.sockets.emit('products', products); //emitir a todos los productos
+		const product = { socketId: socket.id, product: data };
+		products.push(product);
+		const formattedData = JSON.stringify(product);
+		fs.writeFileSync(productsFile, formattedData);
+		io.sockets.emit('products', products);
+	});
+
+	socket.on('new-message', function (data) {
+		const message = { socketId: socket.id, message: data };
+		messages.push(message);
+		const formattedData = JSON.stringify(message);
+		fs.writeFileSync(messagesFile, formattedData);
+		io.sockets.emit('messages', messages);
 	});
 });
 
 app.get('/', (req, res) => res.render('form'));
-
-app.get('/productos', (req, res) => {
-	res.render('products', { products });
-});
-
-app.post('/productos', (req, res) => {
-	let product = req.body;
-	products.push(product);
-	res.redirect('/');
-});
 
 const PORT = 8080;
 const srv = server.listen(PORT, () =>
