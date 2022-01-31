33c728e1f7927f4794da572d0452bdb0e908ffef
Leila Sosa Gonzalez <leisosa93@gmail.com>
2022-01-31 20:38:22 -0300

    read and write on .txt

--- server.js
+++ server.js
@@ -17,31 +17,50 @@ app.use(express.urlencoded({ extended: true }));
 app.use('*/js', express.static('public/js'));
 app.use('*/css', express.static('public/css'));
 
+const readFile = (path) => {
+	let dataFormatted = [];
+	try {
+		const data = fs.readFileSync(path, 'utf-8');
+		if (data) {
+			dataFormatted = JSON.parse(data);
+		}
+	} catch (error) {
+		console.error(error);
+	}
+	return dataFormatted;
+};
+
+const writeFile = (path, item) => {
+	let items = readFile(path);
+	items.push(item);
+	fs.writeFileSync(path, JSON.stringify(items));
+};
+
 io.on('connection', function (socket) {
 	console.log('Conectado!');
+	products = readFile(productsFile);
+	messages = readFile(messagesFile);
 	socket.emit('products', products);
 	socket.emit('messages', messages);
 
 	socket.on('new-product', function (data) {
 		const product = { socketId: socket.id, product: data };
-		products.push(product);
-		const formattedData = JSON.stringify(product);
-		fs.writeFileSync(productsFile, formattedData);
+		writeFile(productsFile, product);
+		products = readFile(productsFile);
 		io.sockets.emit('products', products);
 	});
 
 	socket.on('new-message', function (data) {
 		const message = { socketId: socket.id, message: data };
-		messages.push(message);
-		const formattedData = JSON.stringify(message);
-		fs.writeFileSync(messagesFile, formattedData);
+		writeFile(messagesFile, message);
+		messages = readFile(messagesFile);
 		io.sockets.emit('messages', messages);
 	});
 });
 
 app.get('/', (req, res) => res.render('form'));
 
-const PORT = 8080;
+const PORT = 8082;
 const srv = server.listen(PORT, () =>
 	console.log(`Servidor corriendo en el puerto ${srv.address().port}`)
 );
